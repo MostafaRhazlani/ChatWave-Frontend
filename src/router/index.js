2a3378dd415010316dea55e20d94773bf126195c
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-03-15 13:22:36 +0000

    FRC-60 -> add profile route and authentication guard to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '@/store/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/register',
-      name: 'register',
+      name: 'Register',
       component: () => import('../views/auth/RegisterVue.vue'),
     },
     {
       path: '/login',
-      name: 'login',
+      name: 'Login',
       component: () => import('../views/auth/LoginVue.vue'),
     },
     {
@@ -22,7 +23,40 @@ const router = createRouter({
         showHeader: true
       },
     },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: () => import('../views/pages/ProfileVue.vue'),
+      meta: {
+        requiresAuth: true, 
+        role: 'user', 
+        showSidebar: true,
+        showHeader: true
+      },
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const token = authStore.token;
+  const userRole = authStore.userRole;
+
+  if (to.meta.requiresAuth && !token) {
+    if (to.name !== 'login') {
+      next({ name: 'Login' });
+    } else {
+      next();
+    }
+  } else if (to.meta.role && to.meta.role !== userRole) {
+    if (to.name !== 'Home') {
+      next({ name: 'Home' });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
