8d39c41610b621bd03c5efd0144df15350c95367
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-03-20 12:31:49 +0000

    FRC-78 -> refactor AlertComponent to use Lucide icons and improve styling

--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -1,20 +1,21 @@
 <template>
-    <div v-if="alertVisible" :class="alertClass" class="w-4/5 md:w-3/6 lg:w-2/6 flex items-center justify-between absolute top-2 right-2 px-3 py-2 text-white font-bold rounded-xl">
+    <div v-if="alertVisible" :class="alertClass" class="w-[300px] sm:w-[350px] flex items-center justify-between absolute top-20 md:top-2 z-40 right-2 px-3 py-2 text-white font-bold rounded-xl">
         <div class="flex items-center gap-4">
-            <div class="px-2 py-1 drop-shadow-[0px_0px_2px_rgba(255,255,255)] text-2xl rounded-xl"
+            <div class="w-8 h-8 drop-shadow-[0px_0px_2px_rgba(255,255,255)] text-2xl rounded-xl flex items-center justify-center"
                 :class="iconClass">
-                <span><i :class="icon"></i></span>
+                <span><component :is="icon" :size="18" :stroke-width="3"/></span>
             </div>
             <h1 class="text-md font-semibold text-gray-800" v-if="alertMessage">{{ alertMessage }}</h1>
         </div>
-        <div @click="clearAlert" class="px-1 rounded duration-300 cursor-pointer hover:bg-opacity-30">
-            <span class="text-gray-800"><i class="fa-solid fa-xmark"></i></span>
+        <div @click="clearAlert" class="rounded duration-300 cursor-pointer hover:scale-125">
+            <span class="text-gray-800"><X :size="17"/></span>
         </div>
     </div>
 </template>
 
 <script setup>
     import { useAlertStore } from '@/store/alert';
+    import { Check, X, TriangleAlert, CircleX } from 'lucide-vue-next';
     import { computed } from 'vue';
 
     const alertStore = useAlertStore();
@@ -34,16 +35,16 @@
     const iconClass = computed(() => {
         return {
             success: "bg-green-400",
-            error: "bg-red-400",
+            error: "bg-red-600",
             warning: "bg-yellow-400",
         }[alertType.value]
     });
 
     const icon = computed(() => {
         return {
-            success: "fa-solid fa-circle-check",
-            error: "fa-solid fa-triangle-exclamation",
-            warning: "fa-solid fa-exclamation-circle",
+            success: Check,
+            error: CircleX,
+            warning: TriangleAlert,
         }[alertType.value]
     });
 
