90ffd282602d6092f157d7edf89db6dd014a0e69
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-05-01 19:22:17 +0100

    FRC-139 -> display all tags in TagsVue component

--- src/views/pages/admin/TagsVue.vue
+++ src/views/pages/admin/TagsVue.vue
@@ -9,15 +9,20 @@
 
             <!-- Search -->
             <div class="bg-slate-800 p-4 rounded-lg mb-6">
-                <div class="flex flex-col md:flex-row gap-4">
-                    <div class="flex-1">
-                        <div class="relative">
-                            <SearchIcon
-                                class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-                            <input type="text" placeholder="Search tags..."
-                                class="w-full bg-slate-700 rounded-lg pl-10 pr-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
-                        </div>
+                <div class="flex flex-col md:justify-between md:flex-row gap-4">
+                    <div class="relative w-3/5">
+                        <SearchIcon
+                            class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+                        <input type="text" placeholder="Search tags..."
+                            class="w-full bg-slate-700 rounded-md pl-10 pr-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
                     </div>
+                    <form>
+                        <div class="flex gap-2">
+                            <input type="text" placeholder="Add new tag"
+                                class="bg-slate-700 rounded-md px-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
+                            <button class="bg-pink-600 hover:bg-pink-500 rounded-md px-4 flex items-center gap-1"><CirclePlus :size="18" /> Add Tag</button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
@@ -34,12 +39,15 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr
+                            <tr v-for="(tag, index) in tags" :key="index" 
                                 class="border-b border-gray-700 hover:bg-gray-750">
-                                <td class="py-3 px-4">1</td>
-                                <td class="py-3 px-4 min-w-36">Sport</td>
-                                <td class="py-3 px-4">2024/02/05</td>
+                                <td class="py-3 px-4">{{ index+1 }}</td>
+                                <td class="py-3 px-4 min-w-36">{{ tag.tag_name }}</td>
+                                <td class="py-3 px-4">{{ tag.created_at }}</td>
                                 <td class="py-3 px-4 text-center">
+                                    <button class="p-1 rounded hover:bg-gray-700" title="Delete">
+                                        <EditIcon class="w-5 h-5 text-gray-400 hover:text-white" />
+                                    </button>
                                     <button class="p-1 rounded hover:bg-gray-700" title="Delete">
                                         <Trash2Icon class="w-5 h-5 text-gray-400 hover:text-red-500" />
                                     </button>
@@ -68,7 +76,29 @@
 </template>
 
 <script setup>
-    import { FileTextIcon, SearchIcon, EyeIcon, Trash2Icon } from 'lucide-vue-next';
+    import axios from 'axios';
+import { FileTextIcon, SearchIcon, EyeIcon, EditIcon, Trash2Icon, CirclePlus } from 'lucide-vue-next';
+    import { onMounted, ref } from 'vue';
+    import { useApiStore } from '@/store/apiStore';
+
+    const tags = ref([]);
+    const apiStore = useApiStore();
+
+    const getAllTags = async () => {
+        apiStore.isLoading = true
+        try {
+            const response = await axios.get('/tags');
+            tags.value = response.data.tags
+        } catch (error) {
+            console.log('Error fetching tags', error);
+        } finally {
+            apiStore.isLoading = false;
+        }
+    }
+
+    onMounted(() => {
+        getAllTags();
+    })
 </script>
 
 
