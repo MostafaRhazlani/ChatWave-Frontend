0382f1d49783cd9edb964a16cec385edc535a7e8
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-05-01 20:40:51 +0100

    FRC-134 -> add SpinnerComponent and integrate loading state in TagsVue for tag creation

--- src/views/pages/admin/TagsVue.vue
+++ src/views/pages/admin/TagsVue.vue
@@ -1,4 +1,6 @@
 <template>
+    <AlertComponent />
+
     <div class="flex h-screen bg-gray-900 text-white">
 
         <!-- Main Content -->
@@ -8,7 +10,7 @@
             </div>
 
             <!-- Search -->
-            <div class="bg-slate-800 p-4 rounded-lg mb-6">
+            <div class="bg-slate-800 p-4  border border-gray-700 rounded-lg mb-6">
                 <div class="flex flex-col md:justify-between md:flex-row gap-4">
                     <div class="relative w-3/5">
                         <SearchIcon
@@ -16,18 +18,21 @@
                         <input type="text" placeholder="Search tags..."
                             class="w-full bg-slate-700 rounded-md pl-10 pr-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
                     </div>
-                    <form>
-                        <div class="flex gap-2">
-                            <input type="text" placeholder="Add new tag"
-                                class="bg-slate-700 rounded-md px-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
-                            <button class="bg-pink-600 hover:bg-pink-500 rounded-md px-4 flex items-center gap-1"><CirclePlus :size="18" /> Add Tag</button>
+                    <form @submit.prevent="createTag">
+                        <div class="flex gap-2 w-3/6 md:w-full">
+                            <input :class="[ error ? 'ring-2 ring-red-600' : '' ]" v-model="formTag.tag_name" type="text" placeholder="Add new tag"
+                                class="w-5/6 bg-slate-700 rounded-md px-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-pink-500"/>
+                            <button class="w-36 px-2 bg-pink-600 hover:bg-pink-500 rounded-md flex items-center justify-center">
+                                <SpinnerComponent v-if="isLoading"/>
+                                <span v-else class="flex items-center gap-1"><CirclePlus :size="18" />Add Tag</span>
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
 
             <!-- Tags Table -->
-            <div class="bg-slate-800 rounded-lg overflow-hidden">
+            <div class="bg-slate-800 rounded-lg border border-gray-700 overflow-hidden">
                 <div class="overflow-x-auto">
                     <table class="w-full">
                         <thead>
@@ -78,21 +83,49 @@
 <script setup>
     import axios from 'axios';
 import { FileTextIcon, SearchIcon, EyeIcon, EditIcon, Trash2Icon, CirclePlus } from 'lucide-vue-next';
-    import { onMounted, ref } from 'vue';
+    import { onMounted, reactive, ref } from 'vue';
     import { useApiStore } from '@/store/apiStore';
+    import AlertComponent from '@/components/AlertComponent.vue';
+    import SpinnerComponent from '@/components/SpinnerComponent.vue';
+    import { useAlertStore } from '@/store/alert';
 
     const tags = ref([]);
+    const formTag = ref({
+        tag_name: '',
+    });
     const apiStore = useApiStore();
+    const alertStore = useAlertStore();
+    const error = ref('');
+    const isLoading = ref(false);
 
-    const getAllTags = async () => {
-        apiStore.isLoading = true
+    const getAllTags = async (showLoader) => {
+        if(showLoader) apiStore.isLoading = true
         try {
             const response = await axios.get('/tags');
             tags.value = response.data.tags
         } catch (error) {
             console.log('Error fetching tags', error);
         } finally {
-            apiStore.isLoading = false;
+            if (showLoader) apiStore.isLoading = false
+        }
+    }
+
+    const createTag = async () => {
+        error.value = '';
+        if(isLoading.value) return;
+        isLoading.value = true;
+        try {
+            const response = await axios.post('tag/store', formTag.value);
+            if(response.status === 200) {
+                await getAllTags(false);
+                alertStore.triggerAlert('Tag created successfully', 'success');
+                formTag.value.tag_name = '';
+            }
+        } catch (err) {
+            error.value = err.response.data.message
+            console.log(error.value);
+        } finally {
+            isLoading.value = false;
         }
     }
 
